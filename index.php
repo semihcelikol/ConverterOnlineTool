5e9c1d6156bc4a8ccbc7af44d2d8a111f25c9997
semihcelikol <semihcelikol@outlook.com>
2018-12-20 11:30:23 +0300

    url change

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="http://semihcelikol.com" target="_blank">About <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="http://semihcelikol.com/camel-case-to-pascal-case/" target="_blank">About <span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
